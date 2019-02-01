6c36de34dccb0f2aec3c199884c156f4b9f82653
Çağatay EMEKCİ <cemekci@innova.com.tr>
2019-02-01 10:55:14 +0300

    Initial Commit

---
+++ RegisterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegisterApp
+//
+//  Created by Çağatay Emekci on 1.02.2019.
+//  Copyright © 2019 Çağatay Emekci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RegisterAppTests/RegisterAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterAppTests.swift
+//  RegisterAppTests
+//
+//  Created by Çağatay Emekci on 1.02.2019.
+//  Copyright © 2019 Çağatay Emekci. All rights reserved.
+//
+
+import XCTest
+@testable import RegisterApp
+
+class RegisterAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
